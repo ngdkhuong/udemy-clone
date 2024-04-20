6fda1b95004a12b7c70c20a779dd3b4ab6c2f78e
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-04-20 14:48:45 +0700

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,52 @@
+
+import SliderMain from './components/SliderMain'
+import getAllCourses from './actions/getAllCourses';
+import CourseComponent from './components/CourseComponent';
+import myUser from './actions/getUser';
+
+const images = [
+  "/a.jpg",
+  "/b.jpg",
+]
+
+
+interface HomeProps {
+  searchParams : string
+}
+
+export default async function Home({searchParams}:HomeProps) {
+
+    const courses = await getAllCourses(searchParams); // get all the searched courses from api
+    const currentUser = await myUser();
+
+ 
+
+  return (
+    
+    <main className='w-[100%]'>
+      <SliderMain // ??
+      images={images}
+      />
+
+      <div>
+
+          <div className='flex flex-wrap px-8'>
+            {courses.map((item:any) => (
+              <CourseComponent // ??
+                key={item.id}
+                data={item}   
+                currentUser={currentUser}       
+                
+                />
+            ))}
+
+          
+          </div>
+
+         
+      </div>
+
+
+    </main>
+  )
+}
